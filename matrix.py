44f7dc7d0f1646e895c38e2994203efb0924c861
pomarka <33197151+pomarka@users.noreply.github.com>
2019-04-02 18:43:41 +0200

    add second version

--- matrix.py
+++ matrix.py
@@ -1,15 +1,109 @@
-# -*- coding: utf-8 -*- 
+import math
+from copy import deepcopy
+
 
 class Matrix:
-    size_of_matrix = 2
-    matrix = [[0 for col in range(size_of_matrix)] for row in range(size_of_matrix)]
+    def __init__(self, *args, **kwargs):
+        N = math.sqrt(len(args))
+        if math.floor(N) != N:
+            raise ValueError("wrong number of arguments")
+        self.N = int(N)
+        self.matrix = [[] for x in range(self.N)]
+        for i, x in enumerate(args):
+            self.matrix[i // self.N].append(x)
 
-    def __init__(a, b, c, d):
-        matrix[0][0] = a
-        matrix[0][0] = b
-        matrix[0][0] = c
-        matrix[0][0] = d
-    
-    def printMatrix(self):
-        for i in self.matrix:
-            print(i)
+    def __str__(self):
+        result = "["
+        flattened = [x for y in self.matrix for x in y]
+        longest_num_chars = len(str(max(flattened)))
+        for i, row in enumerate(self.matrix):
+            for j, column in enumerate(row):
+                if j == 0 and i > 0: # wyrównanie przed pierwsza kolumną
+                    result += " "
+                result += str(column)
+                if j != (len(row)-1):
+                    result += ", "
+                    result += " " * (longest_num_chars - len(str(column))) # wyrównanie większych liczb
+                elif i != (len(self.matrix)-1): # newline po każdym N elementów
+                    result += "\n"
+
+        result += "]"
+        return result
+
+    def __copy__(self):
+        m = Matrix()
+        m.N = self.N
+        m.matrix = deepcopy(self.matrix)
+        return m
+
+    def __iadd__(self, other):
+        if isinstance(self, Matrix) and isinstance(other, Matrix):
+            if other.N != self.N:
+                raise ValueError("matrices with different dimensions")
+
+        for i in range(self.N):
+                for j in range(self.N):
+                    self.matrix[i][j] += other.matrix[i][j]
+
+        return self
+
+    def __isub__(self, other):
+        if isinstance(self, Matrix) and isinstance(other, Matrix):
+            if other.N != self.N:
+                raise ValueError("matrices with different dimensions")
+
+        for i in range(self.N):
+                for j in range(self.N):
+                    self.matrix[i][j] -= other.matrix[i][j]
+
+        return self
+
+    def __radd__(self, i):
+        return self + i
+
+    def __add__(self, other):
+        if isinstance(self, Matrix) and isinstance(other, Matrix):
+            if other.N != self.N:
+                raise ValueError("matrices with different dimensions")
+            m = self.__copy__()
+
+            for i in range(self.N):
+                for j in range(self.N):
+                    m.matrix[i][j] += other.matrix[i][j]
+
+            return m
+
+        if isinstance(self, Matrix) and isinstance(other, int):
+            m = self.__copy__()
+
+            for i in range(self.N):
+                for j in range(self.N):
+                    m.matrix[i][j] += other
+
+            return m
+
+    def __sub__(self, other):
+        if other.N != self.N:
+            raise ValueError("matrices with different dimensions")
+        m = self.__copy__()
+
+        for i in range(self.N):
+            for j in range(self.N):
+                m.matrix[i][j] -= other.matrix[i][j]
+
+        return m
+
+    def __matmul__(self, other):
+       if other.N != self.N:
+            raise ValueError("matrices with different dimensions")
+       tmp_list = []
+       s = 0
+
+       for x in range(self.N):
+           for y in range(self.N):
+               for k in range(self.N):
+                   s += self.matrix[x][k] * other.matrix[k][y]
+               tmp_list.append(s)
+               s = 0
+
+       return Matrix(*tmp_list)
--- task.py
+++ task.py
@@ -1,25 +1,35 @@
-#Write a library that contains a class that can represent any 2𝑥2 real matrice. 
-#Include two functions to calculate the sum and product of two matrices. 
-#Next, write a program that imports this library module and use it to perform calculations.
-#Examples:
-#
-# matrix_1 = Matrix(4,5,6,7)
-# matrix_2 = Matrix(2,2,2,1)
-#
-# matrix_3 = matrix_2.add(matrix_1)
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#(If you want you can expand implementation to NxN matrix.)
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+from matrix import Matrix
+
+if __name__ == '__main__':
+    m = Matrix(2, 2, 3, 4)
+    m2 = Matrix(1, 1, 1, 1)
+
+    print('First matrix: m:')
+    print(m)
+
+    print('Second matrix: m2:')
+    print(m2)
+
+    print('Adding matrixes: m3 = m + m2:')
+    m3 = m + m2
+    print(m3)
+
+    print('Substracting matrixes: m4 = m - m2:')
+    m4 = m - m2
+    print(m4)
+
+    print('Adding one matrix to another: m += m2:')
+    m += m2
+    print(m)
+
+    print('Substracting one matrix from another: m -= m2:')
+    m -= m2
+    print(m)
+
+    print('Adding value to matrix elements: m5 = m + 2')
+    m5 = m + 2
+    print(m5)
+
+    print('Adding value to matrix elements: m6 = 2 + m')
+    m6 = 2 + m
+    print(m6)
